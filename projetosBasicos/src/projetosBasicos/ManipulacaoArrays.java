7951e0aaa33f77584ec8ddd78191e355930d1685
Rafael Barbosa Da Silva <119977773+snnayingnic@users.noreply.github.com>
2024-04-13 22:40:47 -0300

    Add files via upload

---
+++ projetosBasicos/src/projetosBasicos/ManipulacaoArrays.java
@@ -0,0 +1,15 @@
+package projetosBasicos;
+import java.util.ArrayList;
+
+public class ManipulacaoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int[] numeros= {1,2,4,6,7};
+			
+		
+System.out.println(numeros[1]);
+	}
+
+}
