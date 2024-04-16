16de701d055e3b019a0c885b7b3db3a8b90be5d9
Rafael Barbosa Da Silva <119977773+snnayingnic@users.noreply.github.com>
2024-04-15 22:13:45 -0300

    Add files via upload

---
+++ projetosBasicos/projetosBasicos/ManipulacaoArrays.java
@@ -0,0 +1,20 @@
+package projetosBasicos;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ManipulacaoArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	// criar uma lista de compras
+		int[] numeros= {1,2,4,6,7};
+		String[] listaDeCompras= {"arroz","massa de Milho","feijão","alho","cebola","sal"};
+			System.out.println("LIsta");
+			System.out.println(listaDeCompras.length);
+			for(int i=0;i<=6;i++) {
+			System.out.println(listaDeCompras[i]);
+			}
+System.out.println(numeros[1]);
+	}
+
+}
